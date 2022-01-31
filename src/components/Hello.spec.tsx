bfe2e9bc6f53c184abd27b1f1f8ce9864e77ab1d
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2022-01-30 22:14:06 -0600

    Working

--- src/components/Hello.spec.tsx
+++ src/components/Hello.spec.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import React from "react";
 import Hello from "./Hello";
 
-it("renders Hello world", () => {
+it('renders "Hello World"', () => {
   render(<Hello />);
-  const ele = screen.getByText(/Hello world/);
-  screen.debug();
-  expect(ele).toBeInTheDocument();
+  const element = screen.getByText(/Hello World/);
+  expect(element).toBeInTheDocument();
 });
